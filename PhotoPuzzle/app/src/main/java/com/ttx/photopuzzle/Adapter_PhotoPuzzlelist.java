09d433c1f4106ed0b39301cbdd61dc2ede63f53a
paul <paul>
2017-06-27 21:24:30 +0530

    puzzle list page with adapter view

---
+++ PhotoPuzzle/app/src/main/java/com/ttx/photopuzzle/Adapter_PhotoPuzzlelist.java
@@ -0,0 +1,72 @@
+package com.ttx.photopuzzle;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by SONY on 27-06-2017.
+ */
+
+public class Adapter_PhotoPuzzlelist extends BaseAdapter {
+
+
+    Context context;
+    List<PhotoPuzzleListcls> list1;
+
+    public Adapter_PhotoPuzzlelist(Context context, List<PhotoPuzzleListcls> list1) {
+        this.context = context;
+        this.list1 = list1;
+
+    }
+
+    @Override
+    public int getCount() {
+        return list1.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder viewHolder;
+        if(view==null){
+            viewHolder=new ViewHolder();
+            view= LayoutInflater.from(context).inflate(R.layout.custom_photopuzzlelist, null, false);
+            viewHolder.tv1=(TextView)view.findViewById(R.id.tv_listno);
+            viewHolder.tv2=(TextView)view.findViewById(R.id.tv_unlock);
+
+            view.setTag(viewHolder);
+
+        }
+        else{
+            viewHolder=(ViewHolder)view.getTag();
+        }
+
+
+        viewHolder.tv1.setText(list1.get(i).getNo());
+        viewHolder.tv2.setText(list1.get(i).getUnlock());
+        viewHolder.tv2.setText(list1.get(i).getLock());
+        return view;
+
+    }
+    class  ViewHolder{
+       TextView tv1;
+        TextView tv2;
+    }
+
+}
---
+++ PhotoPuzzle/app/src/main/java/com/ttx/photopuzzle/PhotoPuzzleListcls.java
@@ -0,0 +1,44 @@
+package com.ttx.photopuzzle;
+
+/**
+ * Created by SONY on 27-06-2017.
+ */
+
+public class PhotoPuzzleListcls {
+    String name;
+    String lock;
+    String unlock;
+    int no;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLock() {
+        return lock;
+    }
+
+    public void setLock(String lock) {
+        this.lock = lock;
+    }
+
+    public String getUnlock() {
+        return unlock;
+    }
+
+    public void setUnlock(String unlock) {
+        this.unlock = unlock;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+}
