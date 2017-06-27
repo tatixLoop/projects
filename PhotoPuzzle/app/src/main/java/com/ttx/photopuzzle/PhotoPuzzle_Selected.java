004807f85c4904d3cd54787cde994f3b7f328a12
paul <paul>
2017-06-27 23:07:29 +0530

    photopuzzle_selected page is retrived

--- PhotoPuzzle/app/src/main/java/com/ttx/photopuzzle/PhotoPuzzle_Selected.java
+++ PhotoPuzzle/app/src/main/java/com/ttx/photopuzzle/PhotoPuzzle_Selected.java
@@ -1,40 +1,116 @@
 package com.ttx.photopuzzle;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class PhotoPuzzle_Selected extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhotoPuzzle_Selected extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     TextView tv_puzzleselected;
+    ListView lv;
+    List<PhotoPuzzleListcls> list1;
+    PhotoPuzzle_Data db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_puzzle__selected);
+        lv = (ListView) findViewById(R.id.lv_list);
 
+        tv_puzzleselected = (TextView) findViewById(R.id.tv_name);
 
-        tv_puzzleselected= (TextView) findViewById(R.id.tv_name);
+        int type = getIntent().getIntExtra("puzzleselected", 0);
 
-        int type = getIntent().getIntExtra("puzzleselected",0);
-
-        if(type == 2)
+        if (type == 2)
             tv_puzzleselected.setText("2x2");
 
-        if(type == 3)
+        if (type == 3)
             tv_puzzleselected.setText("3x3");
 
-        if(type == 4)
+        if (type == 4)
             tv_puzzleselected.setText("4x4");
 
-        if(type == 5)
+        if (type == 5)
             tv_puzzleselected.setText("5x5");
 
-        Log.d("PS","type = "+ tv_puzzleselected.getText().toString());
-       /* tv_puzzleselected.setText(message);*/
+        Log.d("PS", "type = " + tv_puzzleselected.getText().toString());
+
+
+        list1 = new ArrayList<>();
+
+        db = new PhotoPuzzle_Data(this);
+        db.openConnection();
+        getpuzzlelist();
 
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+       ////// go to page PhotoPuzzle_game
+
+
+/*
+
+        int number = list1.get(i).getNo();
+
+        Intent intent = new Intent(PhotoPuzzle_Selected.this, PushupDetails.class);
+
+        intent.putExtra("fid", number);
+        intent.putExtra("position",i);
+        startActivity(intent);
+*/
+
+
+
+    }
+
+
+    private void getpuzzlelist() {
+
+
+        String se = "select * from tb_photopuzzle";
+
+        Cursor c = db.selectData(se);
+        if (c != null) {
+            while (c.moveToNext()) {
+
+                String lok = c.getString(0);
+                String unlok = c.getString(1);
+                int n = c.getInt(2);
+                //int img = c.getInt(3);
+                PhotoPuzzleListcls p1 = new PhotoPuzzleListcls();
+                p1.setNo(n);
+                p1.setLock(lok);
+                p1.setUnlock(unlok);
+                list1.add(p1);
+
+            }
+
+
+            Adapter_PhotoPuzzlelist a = new Adapter_PhotoPuzzlelist(this, list1);
+            lv.setAdapter(a);
+            lv.setOnItemClickListener(this);
+
+
+        }
+
+
+    }
 }
