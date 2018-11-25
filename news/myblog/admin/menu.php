d183e47cc3ecad27d2b3523c969633af2d12bc28
jithin <jithin01@gmail.com>
2018-11-25 16:19:07 +0530

    changing header view for admin page

--- news/myblog/admin/menu.php
+++ news/myblog/admin/menu.php
@@ -2,12 +2,19 @@
 <div class="navbar-fixed">
 <nav>
     <div class="nav-wrapper">
-      <a href="#!" class="brand-logo center"><img src="../assets/images/logo.png" class="logo_img"></a>
+      <a href="#!" class="brand-logo center"><img src="../assets/images/logo_arpo.png" class="logo_img"></a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down" id='adminmenu'>
-       <li><a href='index.php'>Blog</a></li>
+       <li><a href='index.php'>Articles</a></li>
        <li><a href="gallery.php">Gallery</a></li>
+<?php
+if ($_SESSION['access'] == 15)
+{
+?>
 	<li><a href='users.php'>Users</a></li>
+<?php
+}
+?>
 	<li><a href="../" target="_blank">View Website</a></li>
 	<li><a href='logout.php'>Logout</a></li>
       </ul>
@@ -16,9 +23,16 @@
   </div>
 
   <ul class="sidenav" id="mobile-demo">
-    <li><a href='index.php'>Blog</a></li>
+    <li><a href='index.php'>Articles</a></li>
     <li><a href="gallery.php">Gallery</a></li>
+<?php
+if ($_SESSION['access'] == 15)
+{
+?>
 	<li><a href='users.php'>Users</a></li>
+<?php
+}
+?>
 	<li><a href="../" target="_blank">View Website</a></li>
 	<li><a href='logout.php'>Logout</a></li>
   </ul>
